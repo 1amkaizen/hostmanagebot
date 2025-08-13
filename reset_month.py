9f991ce87ca55cdbb08ef81a5c66897da141dcb0
1amkaizen <1amkaiz3n@gmail.com>
2025-08-13 23:44:45 +0700

    update

--- reset_month.py
+++
@@ -1,7 +0,0 @@
-# 📍 File: test_reset.py
-from datetime import date
-from handlers.payment_reminder import reset_monthly_status
-
-# Simulasi 1 September 2025
-reset_monthly_status(simulated_today=date(2025, 9, 1))
-
